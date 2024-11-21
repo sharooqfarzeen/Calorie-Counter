3c49cad92e459bce0b8ab052e1f6044c5fe334c0
sharooqfarzeen <sharooqfarzeen@gmail.com>
2024-11-21 16:14:38 +0530

    app runs only with api keys

--- app.py
+++ app.py
@@ -35,57 +35,59 @@ if "GOOGLE_API_KEY" not in st.session_state:
         st.session_state["GOOGLE_API_KEY"] = os.environ["GOOGLE_API_KEY"]
     st.session_state["chat_session"] = start_chat_session()
 
-# Header
-st.title("Calorie Counter")
 
-# Initializing chat history
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+if "GOOGLE_API_KEY" in st.session_state:
+    # Header
+    st.title("Calorie Counter")
 
-# Display chat messages from history on app rerun
-for message in st.session_state["messages"]:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+    # Initializing chat history
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = []
 
-# Sidebar for text input and image upload
-with st.sidebar.form(key="chat_form", clear_on_submit=True):
-    st.header("Upload Image")
-    # Get resume input from user
-    file_object = st.file_uploader("Upload", type=["jpg", "jpeg", "png"])
-    # Submit Button
-    submit_file = st.form_submit_button("Count")
+    # Display chat messages from history on app rerun
+    for message in st.session_state["messages"]:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
-text = st.chat_input(placeholder="Get more insights.")
+    # Sidebar for text input and image upload
+    with st.sidebar.form(key="chat_form", clear_on_submit=True):
+        st.header("Upload Image")
+        # Get resume input from user
+        file_object = st.file_uploader("Upload", type=["jpg", "jpeg", "png"])
+        # Submit Button
+        submit_file = st.form_submit_button("Count")
 
-# React to user input
-if submit_file or text:
-        input = []
-        # If prompt has a file attachment
-        if submit_file and file_object:
-            with st.spinner("Processing your image..."):
-                    # Extract image file from the attached file_object
-                    file = Image.open(file_object)
-                    # Show image in chat
-                    st.chat_message("user").image(file)
-                    # Add user message to chat history
-                    st.session_state.messages.append({"role": "user", "content": file})
-                    # Add image to model input
-                    input.append(file)
-    
-        # If prompt is only text
-        if text:
-            # Content to be given to Model
-            input.append(text)
-            # Display user message in chat message container
-            st.chat_message("user").markdown(text)
-            # Add user message to chat history
-            st.session_state.messages.append({"role": "user", "content": text})
-        else:
-             input.append("Act according to system instruction provided.")
+    text = st.chat_input(placeholder="Get more insights.")
 
-        if input:
-                with st.spinner(text="Analyzing..."):
-                    # Display assistant response in chat message container
-                    response = st.chat_message("assistant").write_stream(stream=get_response(input, st.session_state["chat_session"]))
-                    # Add assistant response to chat history
-                    st.session_state.messages.append({"role": "assistant", "content": response})
+    # React to user input
+    if submit_file or text:
+            input = []
+            # If prompt has a file attachment
+            if submit_file and file_object:
+                with st.spinner("Processing your image..."):
+                        # Extract image file from the attached file_object
+                        file = Image.open(file_object)
+                        # Show image in chat
+                        st.chat_message("user").image(file)
+                        # Add user message to chat history
+                        st.session_state.messages.append({"role": "user", "content": file})
+                        # Add image to model input
+                        input.append(file)
+        
+            # If prompt is only text
+            if text:
+                # Content to be given to Model
+                input.append(text)
+                # Display user message in chat message container
+                st.chat_message("user").markdown(text)
+                # Add user message to chat history
+                st.session_state.messages.append({"role": "user", "content": text})
+            else:
+                input.append("Act according to system instruction provided.")
+
+            if input:
+                    with st.spinner(text="Analyzing..."):
+                        # Display assistant response in chat message container
+                        response = st.chat_message("assistant").write_stream(stream=get_response(input, st.session_state["chat_session"]))
+                        # Add assistant response to chat history
+                        st.session_state.messages.append({"role": "assistant", "content": response})
